a2eeb565a34c994553a4ad4c94a91a18ed14b4ca
asha joseph <ashajo183@gmail.com>
2021-02-23 09:33:59 +0100

    StudentConfigClass

---
+++ src/main/java/se/lexicon/config/StudentConfig.java
@@ -0,0 +1,10 @@
+package se.lexicon.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "se.lexicon")
+public class StudentConfig {
+
+}
